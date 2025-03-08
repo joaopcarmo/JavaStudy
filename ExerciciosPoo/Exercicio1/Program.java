4f7e32391a6fc8dcd7522f9244a972a563ee9bf7
João Pedro Carmo <142856277+joaopcarmo@users.noreply.github.com>
2025-03-07 21:02:58 -0300

    Exercicios de POO na linguagem Java.

---
+++ ExerciciosPoo/Exercicio1/Program.java
@@ -0,0 +1,26 @@
+package ExerciciosPoo.Exercicio1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rect = new Rectangle();
+		
+		System.out.println("Enter rectangle width and height:");
+		rect.width = sc.nextDouble();
+		rect.height = sc.nextDouble();
+		
+		System.out.println("AREA = "+rect.area());
+		System.out.println("PERIMETER = "+rect.perimeter());
+		System.out.println("DIAGONAL = "+rect.diagonal());
+
+		
+
+	}
+
+}
